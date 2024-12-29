88a95cd7f7401e5770f3edacb2a291fd0ef2f31f
Thiyagubaskaran <111138853+Thiyagubaskaran@users.noreply.github.com>
2024-12-29 11:59:50 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API Aggregator!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
